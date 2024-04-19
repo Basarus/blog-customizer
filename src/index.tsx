8fa25a2642e3367c0374d8d64d1e182a0d75c968
basarus51 <basarus51@gmail.com>
2024-04-19 22:20:14 +0300

    review

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import {
+	StrictMode,
+	CSSProperties,
+	useState,
+	useContext,
+	createContext,
+} from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+
+import { defaultArticleState, OptionType } from 'src/constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +19,50 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export interface IAppContext {
+	FontFamily: OptionType;
+	FontSize: OptionType;
+	FontColor: OptionType;
+	BackgroundColor: OptionType;
+	ContentWidth: OptionType;
+}
+
+export const AppContext = createContext({} as IAppContext);
+
 const App = () => {
+	const [seed, setSeed] = useState(1);
+
+	const appContext = useContext(AppContext);
+
+	function acceptState() {
+		setSeed(Math.random());
+	}
+
+	appContext.FontFamily =
+		appContext.FontFamily ?? defaultArticleState.fontFamilyOption;
+	appContext.FontSize =
+		appContext.FontSize ?? defaultArticleState.fontSizeOption;
+	appContext.FontColor = appContext.FontColor ?? defaultArticleState.fontColor;
+	appContext.ContentWidth =
+		appContext.ContentWidth ?? defaultArticleState.contentWidth;
+	appContext.BackgroundColor =
+		appContext.BackgroundColor ?? defaultArticleState.backgroundColor;
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appContext.FontFamily.value,
+					'--font-size': appContext.FontSize.value,
+					'--font-color': appContext.FontColor.value,
+					'--container-width': appContext.ContentWidth.value,
+					'--bg-color': appContext.BackgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<AppContext.Provider value={appContext}>
+				<ArticleParamsForm key={seed} acceptState={acceptState} />
+			</AppContext.Provider>
 			<Article />
 		</div>
 	);
